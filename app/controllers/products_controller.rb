89423c2a1b4a5677d53764dd7e9cf2be2d4e0b3f
kamelb42000 <bbs42000@hotmail.fr>
2024-05-04 12:46:48 +0200

    product in progesse

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,9 +5,47 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user_id = current_user.id
+    @product.photos.attach(params[:product][:photos])
+    if @product.save
+      return redirect_to products_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+    redirect_to products_path(@products), flash: {alert: "Le produit a été modifié avec succès"}
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, status: :see_other
+  end
+
   private
 
-  def stuff_params
-    params.require(:product).permit(:name, :price, :adress, description:, photos: [] )
+  def product_params
+    params.require(:product).permit(:name, :price, :adress, :description, :user_id, photos: [] )
   end
 end
