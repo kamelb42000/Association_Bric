dc132785f97c45a64a30d1503981a92f174ba5a4
kamelb42000 <bbs42000@hotmail.fr>
2024-05-05 12:52:57 +0200

    programming controller booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,15 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+    @my_product_booked = Booking.joins(:product).where("stuffs.user_id = ?", current_user.id)
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :user_id, :stuff_id)
+  end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,25 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  private
+
+  def user_cannot_be_owner
+    if user == product.user
+      errors.add(:user, "le propriétaire du produit ne peut pas le réservé")
+    end
+  end
+
+  def user_only_booked_one_product
+    if user.bookings.any?
+      errors.add(:user, "l'utilisateur ne peut réserver qu'un produit à la fois")
+    end
+  end
+
+  def user_can_book_again
+    if user.bookings.accepted.any?
+      errors.add(:user, "l'utilisateur ne peut réserver un nouveau produit tant que sa réservation actuelle est acceptée")
+    end
+  end
+
 end
